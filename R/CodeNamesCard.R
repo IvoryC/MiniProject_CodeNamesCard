fbd38f64fa68b66a89a137c7a451ab780987c076
Ivory Blakley <ieclabau@uncc.edu>
2017-10-08 01:56:29 -0400

    Pull out the option handling into a separate function, put everything else in main method

--- R/CodeNamesCard.R
+++ R/CodeNamesCard.R
@@ -4,161 +4,33 @@
 
 library("optparse")
 
-# Take arguments from the command line
-#args <- commandArgs(trailingOnly = T)
-option_list = list(
-	make_option(opt_str=c("-h", "--hieght"), type="integer", default=NULL, 
-							help="number of rows in grid"),
-	make_option(opt_str=c("-w", "--width"), type="integer", default=NULL, 
-							help="number of columns in grid"),
-	make_option(opt_str=c("-a", "--assasin"), type="integer", 
-							default=1, 
-							help="number of assisins"),
-	make_option(opt_str=c("-i", "--inocents"), type="integer", default=NULL, 
-							help="number of innocent by-stander squares", dest="ib"),
-	make_option(opt_str=c("-r", "--red"), type="integer", default=NULL, 
-							help="number of red team squares"),
-	make_option(opt_str=c("-b", "--blue"), type="integer", default=NULL, 
-							help="number of blue team squares"),
-	make_option(opt_str=c("-o", "--outfile"), type="character", 
-							default="SpyMap.png", 
-							help="file name to save image"),
-	make_option(opt_str=c("-s", "--set-seed"), type="integer", default=NULL, 
-							help="file name to save image", dest="ss")
-); 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
-
-# for development
-opt
-save("opt", file="testing.Rdata")
-
-# If -s is not null, set the seed.
-if (!is.null(opt$ss)){
-	set.seed(opt$ss)
-}
-
-# if both hieght and width are null, set both to 5.
-# if only one is NULL, set it to match the other.
-if (is.null(opt$hieght) & is.null(opt$width)){
-	opt$hieght=5
-	opt$width=5
-} else {
-	if (is.null(opt$hieght)) {
-		opt$hieght = opt$width
-	}
-	if (is.null(opt$width)) {
-		opt$width = opt$hieght
-	}
-}
-
-# Calculate total squares
-ts = opt$width * opt$hieght
-
-# Notice that the order of handling cases matters
-# first - if both blue and red are specified
-# second - if neighter are specified 
-#     (if they were specified, this is correctly skipped)
-# last (as an else from the second) - if one is null
-#     (where we can assume that the other is NOT null)
-
-# if neither -r nor -b is null
-if (!is.null(opt$blue) & !is.null(opt$red)){
-	# calculate open squares, total - assasins - red - blue
-	open = ts - opt$assasin - opt$blue - opt$red
-	if (open < 0) {
-		stop("Not enough squares in the grid.\n")
-	}
-	# if -i is specified,
-	# if -i == open squares ---> great
-	# if -i != open squares, error message
-	if ( (!is.null(opt$ib)) & (opt$ib != open) ) {
-		stop("This doesn't add up.")
-	}
-	# if -i is null, set it equal to open squares.
-	if (is.null(opt$ib)){
-		opt$ib = open
-	}
-}
+main <- function(){
 	
-
-
-# if both are null, 
-if (is.null(opt$blue) & is.null(opt$red)){
-	#if -i is null, calculate -i as 25% of total squares
-	if (is.null(opt$ib)){
-		opt$ib = floor(ts * .25)
-	}
-	# Calculate open squares
-	open = ts - opt$assasin - opt$ib
-	#if open squares < 3 then print error message
-	if (open < 3){
-		stop("Not enough squares in the grid.\n")
-	}
-	## Make sure open squares is odd
-	# One team has to go first, the other team has to have 
-	# a -1 addvantage to make up for it.
-	# If the number of open squares is even, 
-	# make it odd by adding or subtracting one innocent
-	if (open %% 2 == 0){
-		if (opt$ib > open/2){
-			opt$ib = opt$ib - 1
-			open = open + 1
-		} else {
-			opt$ib = opt$ib + 1
-			open = open - 1
-		}
-	}
-	# randomly pick red or blue to be "first", the other second
-	sample = sample(c("blue", "red"), size=2, replace = F)
-	first = sample[1]
-	second = sample[2]
-	# assign first to get open squares /2 rounded up
-	opt[[first]] = ceiling(open/2)
-	# assign second to get open squares /2 roudned down
-	opt[[second]] = floor(open/2)
-} else {
+opt = processOptions()
 	
-#if exactly one of red or blue is null, 
-	# assign the one that is not null to be first, the other second
-	# assign second to get first - 1 squares
-	if (is.null(opt$blue)){
-		first="red"
-		second="blue"
-		opt$blue = opt$red - 1
-	}
-	if (is.null(opt$red)){
-		first="blue"
-		second="red"
-		opt$red = opt$blue - 1
-	}
-	# Calculate open squares, total - assasins - red - blue
-	open = ts - opt$assasins - opt$red - opt$blue
-	#if <0, error message
-	if (open < 0) {
-		stop("Not enough squares in the grid.\n")
-	}
-	if (open == 0) {
-		warning("There are no inocent by-standers on this map.\n", call.=F)
-	}
-	# if -i is null, set -i to open squares
-	if (is.null(opt$ib)){
-		opt$ib = open
-	}
-	# if -i is specified and -i != open squares, error message
-	if ( (!is.null(opt$ib)) & (opt$ib != open)){
-		stop("This doesn't add up.")
-	}
-}
-	
+	ts = opt$width * opt$hieght
 
 # for developemnt, print number of 
 # total squares in grid, -h and -w
 # assasins, red, blue, by-standers
+print(paste("total squares in grid:", ts))
+#cat(unlist(opt), sep='\n')
+for (i in 1:length(opt)){
+	print(paste0(names(opt)[i],": ", opt[[i]]))
+}
+
+attach(opt) # so we don't have to write opt$ every time
 
 ### Assign squares in grid
-
-# assign squares to grid locaitons, random
+slotIds = sample(x=1:ts, size=ts, replace=F)
+names(slotIds) = c(rep("assasin", assasin),
+											 rep("ib", ib),
+											 rep("blue", blue),
+											 rep("red", red))
+groups = split(slotIds, f=names(slotIds))
+columns = slotIds %% width +1
+rows = (slotIds-1) %/% width +1
+											 
 
 
 ### Make image
@@ -181,6 +53,159 @@ if (is.null(opt$blue) & is.null(opt$red)){
 
 
 
+}
+
+### Functions
+processOptions <- function(opt){
+	# Take arguments from the command line
+	#args <- commandArgs(trailingOnly = T)
+	option_list = list(
+		make_option(opt_str=c("-h", "--hieght"), type="integer", default=NULL, 
+								help="number of rows in grid"),
+		make_option(opt_str=c("-w", "--width"), type="integer", default=NULL, 
+								help="number of columns in grid"),
+		make_option(opt_str=c("-a", "--assasin"), type="integer", 
+								default=1, 
+								help="number of assisins"),
+		make_option(opt_str=c("-i", "--inocents"), type="integer", default=NULL, 
+								help="number of innocent by-stander squares", dest="ib"),
+		make_option(opt_str=c("-r", "--red"), type="integer", default=NULL, 
+								help="number of red team squares"),
+		make_option(opt_str=c("-b", "--blue"), type="integer", default=NULL, 
+								help="number of blue team squares"),
+		make_option(opt_str=c("-o", "--outfile"), type="character", 
+								default="SpyMap.png", 
+								help="file name to save image"),
+		make_option(opt_str=c("-s", "--set-seed"), type="integer", default=NULL, 
+								help="file name to save image", dest="ss")
+	); 
+	opt_parser = OptionParser(option_list=option_list)
+	opt = parse_args(opt_parser)
+	
+	# for development
+	opt
+	save("opt", file="testing.Rdata")
+	
+	# If -s is not null, set the seed.
+	if (!is.null(opt$ss)){
+		set.seed(opt$ss)
+	}
+	
+	# if both hieght and width are null, set both to 5.
+	# if only one is NULL, set it to match the other.
+	if (is.null(opt$hieght) & is.null(opt$width)){
+		opt$hieght=5
+		opt$width=5
+	} else {
+		if (is.null(opt$hieght)) {
+			opt$hieght = opt$width
+		}
+		if (is.null(opt$width)) {
+			opt$width = opt$hieght
+		}
+	}
+	
+	# Calculate total squares
+	ts = opt$width * opt$hieght
+	
+	# Notice that the order of handling cases matters
+	# first - if both blue and red are specified
+	# second - if neighter are specified 
+	#     (if they were specified, this is correctly skipped)
+	# last (as an else from the second) - if one is null
+	#     (where we can assume that the other is NOT null)
+	
+	# if neither -r nor -b is null
+	if (!is.null(opt$blue) & !is.null(opt$red)){
+		# calculate open squares, total - assasins - red - blue
+		open = ts - opt$assasin - opt$blue - opt$red
+		if (open < 0) {
+			stop("Not enough squares in the grid.\n")
+		}
+		# if -i is specified,
+		# if -i == open squares ---> great
+		# if -i != open squares, error message
+		if ( (!is.null(opt$ib)) & (opt$ib != open) ) {
+			stop("This doesn't add up.")
+		}
+		# if -i is null, set it equal to open squares.
+		if (is.null(opt$ib)){
+			opt$ib = open
+		}
+	}
+	
+	
+	
+	# if both are null, 
+	if (is.null(opt$blue) & is.null(opt$red)){
+		#if -i is null, calculate -i as 25% of total squares
+		if (is.null(opt$ib)){
+			opt$ib = floor(ts * .25)
+		}
+		# Calculate open squares
+		open = ts - opt$assasin - opt$ib
+		#if open squares < 3 then print error message
+		if (open < 3){
+			stop("Not enough squares in the grid.\n")
+		}
+		## Make sure open squares is odd
+		# One team has to go first, the other team has to have 
+		# a -1 addvantage to make up for it.
+		# If the number of open squares is even, 
+		# make it odd by adding or subtracting one innocent
+		if (open %% 2 == 0){
+			if (opt$ib > open/2){
+				opt$ib = opt$ib - 1
+				open = open + 1
+			} else {
+				opt$ib = opt$ib + 1
+				open = open - 1
+			}
+		}
+		# randomly pick red or blue to be "first", the other second
+		sample = sample(c("blue", "red"), size=2, replace = F)
+		first = sample[1]
+		second = sample[2]
+		# assign first to get open squares /2 rounded up
+		opt[[first]] = ceiling(open/2)
+		# assign second to get open squares /2 roudned down
+		opt[[second]] = floor(open/2)
+	} else {
+		
+		#if exactly one of red or blue is null, 
+		# assign the one that is not null to be first, the other second
+		# assign second to get first - 1 squares
+		if (is.null(opt$blue)){
+			first="red"
+			second="blue"
+			opt$blue = opt$red - 1
+		}
+		if (is.null(opt$red)){
+			first="blue"
+			second="red"
+			opt$red = opt$blue - 1
+		}
+		# Calculate open squares, total - assasins - red - blue
+		open = ts - opt$assasins - opt$red - opt$blue
+		#if <0, error message
+		if (open < 0) {
+			stop("Not enough squares in the grid.\n")
+		}
+		if (open == 0) {
+			warning("There are no inocent by-standers on this map.\n", call.=F)
+		}
+		# if -i is null, set -i to open squares
+		if (is.null(opt$ib)){
+			opt$ib = open
+		}
+		# if -i is specified and -i != open squares, error message
+		if ( (!is.null(opt$ib)) & (opt$ib != open)){
+			stop("This doesn't add up.")
+		}
+	}
+	return(opt)
+}
 
 
+main()
 

04570c29fed43a16d78143223778f06d3218590a
Ivory Blakley <ieclabau@uncc.edu>
2017-11-14 16:47:04 -0500

    Make CodeNamesCard.R more modular

--- R/CodeNamesCard.R
+++ R/CodeNamesCard.R
@@ -6,98 +6,62 @@ library("optparse")
 
 main <- function(){
 	
-	opt = processOptions()
+  args = getArgs()
+	opt = processOptions(args)
 	
-	ts = opt$width * opt$height
+	#ts = opt$width * opt$height
 	
 	# for developemnt, print number of 
 	# total squares in grid, -h and -w
 	# assassin, red, blue, by-standers
-	print(paste("total squares in grid:", ts))
+	#print(paste("total squares in grid:", ts))
 	#cat(unlist(opt), sep='\n')
 	for (i in 1:length(opt)){
 		print(paste0(names(opt)[i],": ", opt[[i]]))
 	}
 	# TODO add printed statement indicating who should go first
 	
-	attach(opt) # so we don't have to write opt$ every time
+	cardTemplate = assembleCard(opt)
 	
-	### Assign squares in grid
-	slotIds = sample(x=1:ts, size=ts, replace=F)
-	names(slotIds) = c(rep("assassin", assassin),
-										 rep("ib", ib),
-										 rep("blue", blue),
-										 rep("red", red))
-	groups = split(slotIds, f=names(slotIds))
-	columns = slotIds %% width +1
-	rows = (slotIds-1) %/% width +1
-	
-	colors = c(rep("black", assassin),
-						 rep("tan", ib),
-						 rep("blue", blue),
-						 rep("red", red))
-	
-	
-	### Make image
-	# plot Grid
-	# for the image size, assume that each square will be 1 cm square
-	# and a .2 cm gap between each row and each column, 
-	# and that there is a 1 cm border outside of the grid itself.
-	png(filename=opt$outfile)
-	par(mar=rep(1,4))
-	symbols(x=columns, y=rows, squares=rep(.9,ts), inches=F, bg=colors,
-	xaxt="n", yaxt="n")
-	
-	# indicate which team goes first by adding colored rectangles at the border
-	xMin=par("usr")[1]
-	xMax=par("usr")[2]
-	xMid=mean(c(xMin, xMax))
-	locX=c(xMin, xMid, xMax, xMid) #left, top, right, bottom
-	yMin=par("usr")[3]
-	yMax=par("usr")[4]
-	yMid=mean(c(yMin, yMax))
-	locY=c(yMid, yMax, yMid, yMin) #left, top, right, bottom
-	short=.2
-	long=.5
-	symbols(x=locX, y=locY, xpd=T, add=T, inches=F, bg=c(first),
-		rectangles=matrix(nrow=4, ncol=2, 
-			data=cbind(c(short, long, short, long), 
-				c(long, short, long, short))),
-			)
+	drawCard(cardTemplate)
 
-	dev.off()
-	
 	# Show the saved image (works on unix, not sure if this works on windows)
 	system(paste("open", opt$outfile))
 	
 }
 
 ### Functions
-processOptions <- function(opt){
+
+getArgs <- function(){
+  option_list = list(
+    h=make_option(opt_str=c("-H", "--height"), type="integer", default=NULL, 
+                  help="number of rows in grid"),
+    w=make_option(opt_str=c("-W", "--width"), type="integer", default=NULL, 
+                  help="number of columns in grid"),
+    a=make_option(opt_str=c("-a", "--assassin"), type="integer", 
+                  default=1, 
+                  help="number of assissins"),
+    i=make_option(opt_str=c("-i", "--inocents"), type="integer", default=NULL, 
+                  help="number of innocent by-stander squares", dest="ib"),
+    r=make_option(opt_str=c("-r", "--red"), type="integer", default=NULL, 
+                  help="number of red team squares"),
+    b=make_option(opt_str=c("-b", "--blue"), type="integer", default=NULL, 
+                  help="number of blue team squares"),
+    o=make_option(opt_str=c("-o", "--outfile"), type="character", 
+                  default="SpyMap.png", 
+                  help="file name to save image"),
+    s=make_option(opt_str=c("-s", "--set-seed"), type="integer", default=NULL, 
+                  help="set random seed to regenerate an identical card", dest="ss")
+  ); 
+  opt_parser = OptionParser(option_list=option_list)
+  opt = parse_args(opt_parser)
+  
+  return(opt)
+}
+
+processOptions <- function(opt){ # function(opt){
 	# Take arguments from the command line
 	#args <- commandArgs(trailingOnly = T)
-	option_list = list(
-		h=make_option(opt_str=c("-H", "--height"), type="integer", default=NULL, 
-									help="number of rows in grid"),
-		w=make_option(opt_str=c("-W", "--width"), type="integer", default=NULL, 
-									help="number of columns in grid"),
-		a=make_option(opt_str=c("-a", "--assassin"), type="integer", 
-									default=1, 
-									help="number of assissins"),
-		i=make_option(opt_str=c("-i", "--inocents"), type="integer", default=NULL, 
-									help="number of innocent by-stander squares", dest="ib"),
-		r=make_option(opt_str=c("-r", "--red"), type="integer", default=NULL, 
-									help="number of red team squares"),
-		b=make_option(opt_str=c("-b", "--blue"), type="integer", default=NULL, 
-									help="number of blue team squares"),
-		o=make_option(opt_str=c("-o", "--outfile"), type="character", 
-									default="SpyMap.png", 
-									help="file name to save image"),
-		s=make_option(opt_str=c("-s", "--set-seed"), type="integer", default=NULL, 
-									help="set random seed to regenerate an identical card", dest="ss")
-	); 
-	opt_parser = OptionParser(option_list=option_list)
-	opt = parse_args(opt_parser)
 	
 	# for development
 	opt
@@ -123,7 +87,7 @@ processOptions <- function(opt){
 	}
 	
 	# Calculate total squares
-	ts = opt$width * opt$height
+	opt$ts = opt$width * opt$height
 	
 	# Notice that the order of handling cases matters
 	# first - if both blue and red are specified
@@ -137,7 +101,7 @@ processOptions <- function(opt){
 	# if neither -r nor -b is null
 	if (!is.null(opt$blue) & !is.null(opt$red)){
 		# calculate open squares, total - assassin - red - blue
-		openSq = ts - opt$assassin - opt$blue - opt$red
+		openSq = opt$ts - opt$assassin - opt$blue - opt$red
 		if (openSq < 0) {
 			stop("Not enough squares in the grid.\n")
 		}
@@ -159,10 +123,10 @@ processOptions <- function(opt){
 	if (is.null(opt$blue) & is.null(opt$red)){
 		#if -i is null, calculate -i as 25% of total squares
 		if (is.null(opt$ib)){
-			opt$ib = floor(ts * .25)
+			opt$ib = floor(opt$ts * .25)
 		}
 		# Calculate open squares
-		openSq = ts - opt$assassin - opt$ib
+		openSq = opt$ts - opt$assassin - opt$ib
 		#if open squares < 3 then print error message
 		if (openSq < 3){
 			stop("Not enough squares in the grid.\n")
@@ -205,7 +169,7 @@ processOptions <- function(opt){
 			opt$red = opt$blue - 1
 		}
 		# Calculate open squares
-		openSq = ts - opt$assassin - opt$red - opt$blue
+		openSq = opt$ts - opt$assassin - opt$red - opt$blue
 		#if <0, error message
 		if (openSq < 0) {
 			stop("Not enough squares in the grid.\n")
@@ -223,6 +187,74 @@ processOptions <- function(opt){
 		}
 	}
 	return(opt)
+}
+
+
+assembleCard <- function(opt){
+  attach(opt) # so we don't have to write opt$ every time
+  
+  ### Assign squares in grid
+  slotIds = sample(x=1:ts, size=ts, replace=F)
+  names(slotIds) = c(rep("assassin", assassin),
+                     rep("ib", ib),
+                     rep("blue", blue),
+                     rep("red", red))
+  groups = split(slotIds, f=names(slotIds))
+  columns = slotIds %% width +1
+  rows = (slotIds-1) %/% width +1
+  
+  colors = c(rep("black", assassin),
+             rep("tan", ib),
+             rep("blue", blue),
+             rep("red", red))
+  
+  pchs = c(rep(4, assassin), # X
+           rep(NA, ib), # no symbol
+           rep(1, blue), # circle
+           rep(23, red)) # diamond
+  
+  cardTemplate = list(opt=opt,
+                      slotIds=slotIds,
+                      groups=groups,
+                      columns=columns,
+                      rows=rows,
+                      colors=colors)
+  return(cardTemplate)
+}
+
+drawCard <- function(cardTemplate){
+  attach(cardTemplate)
+  attach(opt)
+  
+  ### Make image
+  # plot Grid
+  # for the image size, assume that each square will be 1 cm square
+  # and a .2 cm gap between each row and each column, 
+  # and that there is a 1 cm border outside of the grid itself.
+  png(filename=opt$outfile)
+  par(mar=rep(1,4))
+  symbols(x=columns, y=rows, squares=rep(.9,ts), inches=F, bg=colors,
+          xaxt="n", yaxt="n")
+  
+  # indicate which team goes first by adding colored rectangles at the border
+  xMin=par("usr")[1]
+  xMax=par("usr")[2]
+  xMid=mean(c(xMin, xMax))
+  locX=c(xMin, xMid, xMax, xMid) #left, top, right, bottom
+  yMin=par("usr")[3]
+  yMax=par("usr")[4]
+  yMid=mean(c(yMin, yMax))
+  locY=c(yMid, yMax, yMid, yMin) #left, top, right, bottom
+  short=.2
+  long=.5
+  symbols(x=locX, y=locY, xpd=T, add=T, inches=F, bg=c(first),
+          rectangles=matrix(nrow=4, ncol=2, 
+                            data=cbind(c(short, long, short, long), 
+                                       c(long, short, long, short)))
+  )
+  
+  dev.off()
+  
 }
 
 
